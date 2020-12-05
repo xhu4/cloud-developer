4670b239afeadaba3e027f5a64afc3bf915af22b
Xiukun Hu <xiukun.hu@outlook.com>
2020-12-05 12:19:19 -0800

    basic-server-todos

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -6,12 +6,12 @@ import 'mocha';
 describe('add function', () => {
 
   it('should add two and two', () => {
-    const result = add(2,2);
+    const result = add(2, 2);
     expect(result).to.equal(4);
   });
 
   it('should add -2 and two', () => {
-    const result = add(-2,2);
+    const result = add(-2, 2);
     expect(result).to.equal(0);
   });
 
@@ -20,17 +20,17 @@ describe('add function', () => {
 describe('divide', () => {
 
   it('should divide 6 by 3', () => {
-    const result = divide(6,3);
+    const result = divide(6, 3);
     expect(result).to.equal(2);
   });
 
   it('should divide 5 and 2', () => {
-    const result = divide(5,2);
+    const result = divide(5, 2);
     expect(result).to.equal(2.5);
   });
 
   it('should throw an error if div by zero', () => {
-    expect(()=>{ divide(5,0) }).to.throw('div by 0')
+    expect(() => { divide(5, 0) }).to.throw('div by 0')
   });
 
 });
@@ -38,3 +38,22 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('concat', () => {
+
+  it('should concat "a" and "b"', () => {
+    expect(concat('a', 'b')).to.equal('ab');
+  });
+
+  it('should concat "abc" and "cba"', () => {
+    expect(concat('abc', 'cba')).to.equal('abccba');
+  });
+
+  it('should throw an error if first arg is emtpy', () => {
+    expect(() => { concat('', 'a') }).to.throw('Empty string.')
+  });
+
+  it('should throw an error if second arg is emtpy', () => {
+    expect(() => { concat('b', '') }).to.throw('Empty string.')
+  });
+
+})
